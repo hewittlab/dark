d8cb8ccb86a9016b9a41017777f1fd546b9e3ec4
George Gooden <gecgooden@gmail.com>
2015-05-12 10:44:13 +0800

    Added initial D'ARK details

--- ark-container/src/main/java/au/org/theark/web/pages/Constants.java
+++ ark-container/src/main/java/au/org/theark/web/pages/Constants.java
@@ -20,8 +20,8 @@ package au.org.theark.web.pages;
 
 public class Constants
 {
-	public static final String HOSTED_BY_IMAGE = "hosted_by_image.gif";
-	public static final String PRODUCT_IMAGE = "product_image.png";
+	public static final String HOSTED_BY_IMAGE = "no_study_logo_image.gif";
+	public static final String PRODUCT_IMAGE = "dark_product_image.png";
 	public static final String BANNER_IMAGE = "banner_image.gif";
 	public static final String	STUDY_LOGO_IMAGE	= "study_logo_image.gif";
 	public static final String	NO_STUDY_LOGO_IMAGE	= "no_study_logo_image.gif";
